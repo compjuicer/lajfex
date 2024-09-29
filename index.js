202d8ce486919e9db947b273e207d64a4d392ebd
compjuicer.com <github@compjuicer.com>
2024-09-29 20:28:51 +0100

    adding setColor feature and code cleanup

--- index.js
+++ index.js
@@ -1,66 +1,100 @@
-const Lifx = require('node-lifx-lan');
+const Promise = require("bluebird");
+const Lifx = require("node-lifx-lan");
 
-const l1 = {
-    mac: 'D0:73:D5:2D:5D:36',
-    ip: '192.168.1.7'
-}
+const lights = [
+  {
+    color: "f6019d",
+    address: {
+      mac: "D0:73:D5:2D:5D:36",
+      ip: "192.168.1.9",
+    },
+  },
+  {
+    color: "023788",
+    address: {
+      mac: "D0:73:D5:2D:73:31",
+      ip: "192.168.1.8",
+    },
+  },
+];
 
-const l2 = {
-    mac: 'D0:73:D5:2D:73:31',
-    ip: '192.168.1.8'
-
-};
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8888;
 
 let state = false;
 
 async function main() {
-    const d1 = await Lifx.createDevice(l1);
-    const d2 = await Lifx.createDevice(l2);
+  console.log("lights", lights);
 
-    const turnOn = async (req, res) => {
-        console.log('turn on');
+  const devices = await Promise.mapSeries(lights, (light) => Lifx.createDevice(light.address));
 
-        await d1.turnOn({ color: { css: '#f6019d' } });
-        await d2.turnOn({ color: { css: '#023788' } });
-    };
+  console.log("devices", devices);
 
-    const turnOff = async (req, res) => {
-        console.log('turn off');
+  const turnOn = async () => {
+    console.log("turn on");
 
-        await d1.turnOff();
-        await d2.turnOff();
-    };
+    await Promise.each(devices, (device) => device.turnOn());
+  };
 
-    app.get('/', (req, res) => {
-        state = !state;
+  const turnOff = async () => {
+    console.log("turn off");
 
-        if (state === true) {
-            turnOn();
-        }
-        else if (state === false) {
-            turnOff();
-        }
+    await Promise.each(devices, (device) => device.turnOff());
+  };
 
-        res.end();
-    });
+  const resetColors = async (device) => {
+    console.log("reset colors");
 
-    app.get('/on', async (req, res) => {
-        turnOn();
-        res.end();
-    });
+    await Promise.each(devices, (device, index) => device.setColor({ color: { css: `#${lights[index].color}` }}));
+  };
 
-    app.get('/off', async (req, res) => {
-        turnOff();
-        res.end();
-    });
+  const setColor = async (index, color) => {
+    console.log("set color", index, color);
 
-    app.listen(port, () => {
-        console.log(`lajfex app listening on port ${port}`)
-    });
+    await devices[index].setColor({ color: { css: `#${color}` }});
+  };
+
+  const flip = async () => {
+    state = !state;
+
+    if (state === true) {
+      turnOn();
+    } else if (state === false) {
+      turnOff();
+    }
+  };
+
+  app.get("/", (req, res) => {
+    flip();
+    res.end();
+  });
+
+  app.get("/reset", async (req, res) => {
+    resetColors();
+    res.end();
+  });
+
+  app.get("/set/:index/:color", async (req, res) => {
+    const { index, color } = req.params;
+    setColor(index, color);
+    res.end();
+  });
+
+  app.get("/on", async (req, res) => {
+    turnOn();
+    res.end();
+  });
+
+  app.get("/off", async (req, res) => {
+    turnOff();
+    res.end();
+  });
+
+  app.listen(port, () => {
+    console.log(`lajfex app listening on port ${port}`);
+  });
 }
 
 main();
+
